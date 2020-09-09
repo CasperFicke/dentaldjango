817f2b1221d472a5bda10d2d3daaed39fe615b7f
CasperFicke <cficke@quicknet.nl>
2020-09-09 17:00:02 +0200

    edit git ignore

--- dental/settings.py
+++ dental/settings.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -7,8 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+load_dotenv( BASE_DIR / '.env', )
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'owy%#g*@3q2w=pzx^%o7u*f$p+-5lkfva$m9qkx6vp)qe@std+'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
