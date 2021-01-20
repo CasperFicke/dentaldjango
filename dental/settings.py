6974d865983be5d31b0f564424ae55cfd99047df
CasperFicke <cficke@quicknet.nl>
2021-01-20 17:35:45 +0100

    small edits

--- dental/settings.py
+++ dental/settings.py
@@ -133,7 +133,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'cfidevelopt'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = os.getenv('GMAILPW')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
